f5efcee5a46486fa362c82cf1919e6a8610caca7
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-02-26 21:00:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/Naveen-TG/Dhanush-TG.git /Dhanush-TG
-cd /tgmoviebot
+cd /Dhanush-TG
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
