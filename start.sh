77e87ec36a67abf9a5ee7ef681aba6ed28c2b2b6
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-03-07 15:13:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Naveen-TG/Masterolic.git /Masterolic
-cd /Masterolic
+git clone -b master https://github.com/Naveen-TG/qadirs.git /qadirs
+cd /qadirs
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
